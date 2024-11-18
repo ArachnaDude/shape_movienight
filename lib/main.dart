94169a4f246d3d154608f375c7a45b11957c5c84
Matt <cunning.distraction@googlemail.com>
2024-11-18 00:44:40 +0000

    complete movie list API call

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import "package:flutter/material.dart";
+import "package:provider/provider.dart";
+import "package:flutter_dotenv/flutter_dotenv.dart";
+import "package:http/http.dart" as http;
+import "dart:convert";
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
   runApp(
+    // have to wrap the entire application in change notifier provider
+    // in order for the context to be accessible by everything else
     ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: const MyApp(),
@@ -15,11 +19,11 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MovieNight',
+      title: "MovieNight",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.white,
@@ -28,7 +32,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       // home param sets the page we see when the app starts
-      home: const MovieList(title: 'MovieNight'),
+      home: const MovieList(title: "MovieNight"),
     );
   }
 }
@@ -46,25 +50,188 @@ class MovieList extends StatefulWidget {
   State<MovieList> createState() => _MovieListState();
 }
 
-class _MovieListState extends State<MovieList> {
-  int selectedIndex = 0;
+class Movie {
+  final String title;
+  final String year;
+  final String posterUrl;
+
+  Movie({required this.title, required this.year, required this.posterUrl});
+}
+
+class MovieSearchBar extends StatefulWidget {
+  final Function(String) onSearch;
+  final Function onClear;
+
+  const MovieSearchBar({
+    super.key,
+    required this.onSearch,
+    required this.onClear,
+  });
+
+  @override
+  _MovieSearchBarState createState() => _MovieSearchBarState();
+}
+
+class _MovieSearchBarState extends State<MovieSearchBar> {
+  final TextEditingController _controller = TextEditingController();
+
+  void handleSubmit(String query) {
+    query = query.trim(); // Ensure no extra spaces
+    if (query.isNotEmpty) {
+      widget.onSearch(query); // Call the parent's onSearch function
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    return SearchBar(
+      hintText: 'Search for a movie...',
+      leading: const Icon(Icons.search),
+      controller: _controller,
+      onSubmitted: handleSubmit,
+    );
+  }
+}
+
+class MovieSearchResults extends StatelessWidget {
+  final bool isLoading;
+  final List<Movie> movieList;
+
+  const MovieSearchResults({
+    super.key,
+    required this.isLoading,
+    required this.movieList,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // Show loading indicator
+    if (isLoading) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    // Show "Search for a movie" if no movies are found
+    if (movieList.isEmpty) {
+      return const Center(
+        child: Text("Search for a movie"),
+      );
+    }
+
+    // Show the list of movie cards if movies are found
+    return Expanded(
+      child: ListView.builder(
+        itemCount: movieList.length,
+        itemBuilder: (context, index) {
+          var movie = movieList[index];
+          return Card(
+            margin: const EdgeInsets.symmetric(vertical: 8),
+            child: ListTile(
+              leading: Image.network(
+                movie.posterUrl,
+                width: 50,
+                height: 75,
+                fit: BoxFit.cover,
+              ),
+              title: Text(movie.title),
+              subtitle: Text(movie.year),
+              onTap: () {
+                // Handle card tap (e.g., show movie details)
+                print("Tapped on ${movie.title}");
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _MovieListState extends State<MovieList> {
+  int selectedIndex = 0;
+  String searchQuery = "";
+  bool isLoading = false;
+  List<Movie> movieList = [];
+
+  Future<void> fetchMovies(String query) async {
+    if (query.isEmpty) {
+      setState(() {
+        movieList = [];
+      });
+      return;
+    }
+
+    setState(() {
+      isLoading = true;
+    });
+
+    final url =
+        "http://www.omdbapi.com/?apikey=${dotenv.env['OMDB_API_KEY']}&type=movie&s=$query";
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      if (data["Response"] == "True") {
+        movieList = (data["Search"] as List)
+            .map((movie) => Movie(
+                  title: movie["Title"],
+                  year: movie["Year"],
+                  posterUrl: movie["Poster"],
+                ))
+            .toList();
+      } else {
+        movieList = [];
+      }
+    } else {
+      movieList = [];
+    }
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget movieListPage = Column(
+      children: [
+        MovieSearchBar(
+          onSearch: fetchMovies,
+          onClear: () {
+            setState(() {
+              searchQuery = "";
+              movieList = [];
+            });
+          },
+        ),
+        MovieSearchResults(
+          isLoading: isLoading,
+          movieList: movieList,
+        ),
+      ],
+    );
+
+    // Define the page layout for the Favourites page
+    Widget favouritesPage = const FavouritesPage();
+
     Widget page;
 
+    // Switch between Movie List page and Favourites page
     switch (selectedIndex) {
       case 0:
-        page = const Placeholder();
+        page = movieListPage;
         break;
       case 1:
-        page = const FavouritesPage();
+        page = favouritesPage;
         break;
       default:
-        throw UnimplementedError('No widget for $selectedIndex');
+        throw UnimplementedError("No widget for $selectedIndex");
     }
 
     return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: const [],
+      ),
       body: Row(
         children: [
           SafeArea(
@@ -78,7 +245,7 @@ class _MovieListState extends State<MovieList> {
                 ),
                 NavigationRailDestination(
                   icon: Icon(Icons.bookmark),
-                  label: Text("Favourites"),
+                  label: Text("My Watch List"),
                 ),
               ],
               selectedIndex: selectedIndex,
@@ -105,17 +272,22 @@ class FavouritesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
 
-    // If favouriteMovies is empty, show the message: "List is Empty"
     if (appState.favouriteMovies.isEmpty) {
       return const Center(
-        child:
-            Text("List is Empty"), // Showing a message when the list is empty
+        child: Text(
+          "It's looking pretty empty here...\nTry adding movies from the Movie List page to build your watch list.",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 18),
+        ),
       );
     }
 
-    // If favouriteMovies is not empty, show "Not Empty"
-    return const Center(
-      child: Text("Not Empty"), // This shows when the list is not empty
+    return ListView(
+      children: const [
+        Padding(
+          padding: EdgeInsets.all(20),
+        ),
+      ],
     );
   }
 }
